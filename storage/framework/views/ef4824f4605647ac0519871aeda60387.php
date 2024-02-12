3b5fe5b5cc58dee44d391942dd43837e990ad563
dhruvitsagar19 <159109135+dhruvitsagar19@users.noreply.github.com>
2024-02-12 11:55:11 +0530

    remove

--- storage/framework/views/ef4824f4605647ac0519871aeda60387.php
+++
@@ -1,4 +0,0 @@
-<h1>Forget Password Email</h1>
-   
-You can reset password from bellow link:
-<a href="<?php echo e(route('reset.password.get', $token)); ?>">Reset Password</a><?php /**PATH C:\wamp64\www\sarvopari\resources\views/emails/forgot.blade.php ENDPATH**/ ?>
