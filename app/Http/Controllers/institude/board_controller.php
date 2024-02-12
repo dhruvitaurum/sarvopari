91f67066cb9a3e8eca5500961eaa66c3e618e30b
dhruvitsagar19 <159109135+dhruvitsagar19@users.noreply.github.com>
2024-02-12 11:56:40 +0530

    first

---
+++ app/Http/Controllers/institude/board_controller.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers\institude;
+
+use App\Http\Controllers\Controller;
+use App\Models\board;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class board_controller extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+  public function show(Request $request)
+    {
+        $token = $request->header('Authorization');
+        if (strpos($token, 'Bearer ') === 0) {
+            $token = substr($token, 7);
+        } 
+        
+        
+        $existingUser = User::where('token', $token)->first();
+        if($existingUser){
+            $board = Board::get();
+            $boardData = $board->map(function ($board) {
+                return [
+                    'name' => $board->name,
+                ];
+            });
+            return response()->json([
+                'status' => 200,
+                'message' => 'Successfully fetch data.',
+                'board_list' => $boardData,
+               ], 200, [], JSON_NUMERIC_CHECK);
+        }else{
+            return response()->json([
+                'status' => 400,
+                'message' => 'Invalid token.',
+            ]);        
+        }
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
