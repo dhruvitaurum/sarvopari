2d512e57379c658238ad2163b59ff084746aff81
dhruvitsagar19 <159109135+dhruvitsagar19@users.noreply.github.com>
2024-02-15 17:19:52 +0530

    sidebar changes

--- app/Helpers/menu.php
+++ app/Helpers/menu.php
@@ -1,11 +1,15 @@
 <?php
+
+use App\Models\Menu;
+
 function getDynamicMenu()
 {
-    $menus = \App\Models\Menu::where('sub_menu_id', 0)->orderBy('id', 'asc')->get()->toArray();
-    foreach ($menus as &$menu) {
-        $menu['submenus'] = \App\Models\Menu::where('sub_menu_id', $menu['id'])->orderBy('id', 'asc')->get()->toArray();
-    }
+        $menus = \App\Models\Menu::where('sub_menu_id', 0)->orderBy('id', 'asc')->get()->toArray();
+        foreach ($menus as &$menu) {
+            $menu['submenus'] = \App\Models\Menu::where('sub_menu_id', $menu['id'])->orderBy('id', 'asc')->get()->toArray();
+        }
     return $menus;
+ 
 }
 function get_all_menu_list(){
    return $menus = \App\Models\Menu::orderBy('id', 'asc')->get()->toArray();
