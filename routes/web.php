67cccbfa4f652516f471b4a5deb62a47edd7d8c7
dhruvitsagar19 <159109135+dhruvitsagar19@users.noreply.github.com>
2024-02-20 20:05:57 +0530

    code push

--- routes/web.php
+++ routes/web.php
@@ -132,6 +132,8 @@ Route::middleware('auth')->group(function () {
     Route::get('create/subject-list', [SubjectController::class, 'create_subject'])->name('subject.create');
     Route::POST('get/standard_wise_stream', [SubjectController::class, 'standard_wise_stream'])->name('standard_wise_stream.list');
     Route::post('subject-list/save', [SubjectController::class, 'subject_list_save'])->name('subject_list.save');
+    Route::post('/subject/edit', [SubjectController::class, 'subject_edit'])->name('subject.edit');
+    Route::post('subject/update', [SubjectController::class, 'subject_update'])->name('subject.update');
     Route::post('/subject/delete', [SubjectController::class, 'subject_delete'])->name('subject.delete');
     
     Route::get('/do-business-with/list', [DoBusinessWithController::class, 'list'])->name('do_business_with.list');
