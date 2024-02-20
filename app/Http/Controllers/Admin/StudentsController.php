2817de3ac1821783d8a38e7e50a6f2dae5f87961
PriyankaGitAurumed <159112527+PriyankaGitAurumed@users.noreply.github.com>
2024-02-20 14:56:18 +0530

    studentscontroller
    change in below files
    studentscontroller
    student/create
    student/list
    web

--- app/Http/Controllers/Admin/StudentsController.php
+++ app/Http/Controllers/Admin/StudentsController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\board;
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\institute_for;
+use App\Models\medium;
+use App\Models\class_model;
+use App\Models\stream;
+use App\Models\subject;
+use App\Models\institute_for_sub;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
 
 class StudentsController extends Controller
@@ -15,6 +23,38 @@ class StudentsController extends Controller
     }
 
     public function create_student(){
-        return view('student.create');
+        $id = Auth::id();
+        $formdropdowns['institute_for'] = institute_for::join('institute_for_sub', 'institute_for.id', '=', 'institute_for_sub.institute_for_id')->where('institute_for_sub.institute_id',$id)->get(); 
+        $formdropdowns['board'] = board::join('board_sub', 'board.id', '=', 'board_sub.board_id')->where('board_sub.institute_id',$id)->get();
+        $formdropdowns['medium'] = medium::join('medium_sub', 'medium.id', '=', 'medium_sub.medium_id')->where('medium_sub.institute_id',$id)->get();
+        $formdropdowns['class'] = class_model::join('class_sub', 'class.id', '=', 'class_sub.class_id')->where('class_sub.institute_id',$id)->get();
+        $formdropdowns['stream'] = stream::join('stream_sub', 'stream.id', '=', 'stream_sub.stream_id')->where('stream_sub.institute_id',$id)->get();
+        $formdropdowns['subject'] = subject::join('subject_sub', 'subject.id', '=', 'subject_sub.subject_id')->where('subject_sub.institute_id',$id)->get(); 
+        return view('student.create',compact('formdropdowns'));
+    }
+
+    public function save_student(Request $request){
+
+        $student = User::create([
+        'name' => $request->name,
+        'email' => $request->email,
+        'mobile'=>  $request->mobile,
+        'address'=>$requst->address;
+        'dob'=>$requst->dob;
+        'password' => Hash::make($request->password),
+        'role_type' =>4,
+        ]);
+        $student_id = $student->id;
+        $student = student_detail::create([
+            'student_id' => $student_id,
+            'stage' => $request->stage,
+            'board'=>  $request->board,
+            'medium' =>$request->medium,
+            'class' =>$request->class,
+            'stream'=>$request->stream,
+            'subject'=>$request->subject,
+            ]);
+
+        return Redirect::route('student.list')->with('success', 'profile-created');
     }
 }
--- resources/views/student/create.blade.php
+++ resources/views/student/create.blade.php
@@ -47,25 +47,129 @@
                         </div>
                         <!-- /.card-header -->
                         <!-- form start -->
-                        <form method="post" action="{{ url('institute-for/save') }}">
+                        <form method="post" action="{{ url('student/save') }}">
                             @csrf
                             <div class="card-body">
                                 <div class="form-group">
                                     <div class="row">
-                                        <div class="col-md-6">
+                                        <div class="col-md-3">
                                             <label for="exampleInputEmail1">Name  : </label>
                                             <input type="text" name="name" class="form-control" placeholder="Enter Name">
                                             @error('name')
                                                 <div class="text-danger">{{ $message }}</div>
                                             @enderror
                                         </div>
+
+                                        <div class="col-md-3">
+                                            <label for="exampleInputEmail1">Mobile  : </label>
+                                            <input type="tel" name="mobile" class="form-control" placeholder="Enter Mobile No.">
+                                            @error('mobile')
+                                                <div class="text-danger">{{ $message }}</div>
+                                            @enderror
+                                        </div>
+
+                                        <div class="col-md-3">
+                                            <label for="exampleInputEmail1">EmailID  : </label>
+                                            <input type="email" name="email" class="form-control" placeholder="Enter Email ID">
+                                            @error('email')
+                                                <div class="text-danger">{{ $message }}</div>
+                                            @enderror
+                                        </div>
+
+                                        <div class="col-md-3">
+                                            <label for="exampleInputEmail1">Address  : </label>
+                                            <textarea name="address" class="form-control" placeholder="Address"></textarea>
+                                            @error('address')
+                                                <div class="text-danger">{{ $message }}</div>
+                                            @enderror
+                                        </div>
+
+                                        <div class="col-md-3">
+                                            <label for="exampleInputEmail1">DOB  : </label>
+                                            <input type="date" name="dob" class="form-control" placeholder="Date Of Birth">
+                                            @error('dob')
+                                                <div class="text-danger">{{ $message }}</div>
+                                            @enderror
+                                        </div>
+
+                                        <div class="col-md-3">
+                                            <label for="exampleInputEmail1">Student Stage  : </label>
+                                            <select name="stage">
+                                                @foreach($institute_for as $stage)
+                                                    <option value="{{ $stage->id }}">{{ $stage->name }}</option>
+                                                @endforeach
+                                            </select>
+                                            @error('stage')
+                                                <div class="text-danger">{{ $message }}</div>
+                                            @enderror
+                                        </div>
+
+                                        <div class="col-md-3">
+                                            <label for="exampleInputEmail1">Board  : </label>
+                                            <select name="board">
+                                                @foreach($board as $instituteboard)
+                                                    <option value="{{ $instituteboard->id }}">{{ $instituteboard->name }}</option>
+                                                @endforeach
+                                            </select>
+                                            @error('board')
+                                                <div class="text-danger">{{ $message }}</div>
+                                            @enderror
+                                        </div>
+
+                                        <div class="col-md-3">
+                                            <label for="exampleInputEmail1">Medium : </label>
+                                            <select name="medium">
+                                                @foreach($medium as $institutemedium)
+                                                    <option value="{{ $institutemedium->id }}">{{ $institutemedium->name }}</option>
+                                                @endforeach
+                                            </select>
+                                            @error('medium')
+                                                <div class="text-danger">{{ $message }}</div>
+                                            @enderror
+                                        </div>
+
+                                        <div class="col-md-3">
+                                            <label for="exampleInputEmail1">Student Class  : </label>
+                                            <select name="class">
+                                                @foreach($class as $instituteclass)
+                                                    <option value="{{ $instituteclass->id }}">{{ $instituteclass->name }}</option>
+                                                @endforeach
+                                            </select>
+                                            @error('dob')
+                                                <div class="text-danger">{{ $message }}</div>
+                                            @enderror
+                                        </div>
+
+                                        <div class="col-md-3">
+                                            <label for="exampleInputEmail1">Stream  : </label>
+                                            <select name="stream">
+                                                @foreach($stream as $institutestream)
+                                                    <option value="{{ $institutestream->id }}">{{ $institutestream->name }}</option>
+                                                @endforeach
+                                            </select>
+                                            @error('stream')
+                                                <div class="text-danger">{{ $message }}</div>
+                                            @enderror
+                                        </div>
+
+                                        <div class="col-md-3">
+                                            <label for="exampleInputEmail1">Subject  : </label>
+                                            <select name="subject">
+                                                @foreach($subject as $institutesubject)
+                                                    <option value="{{ $institutesubject->id }}">{{ $institutesubject->name }}</option>
+                                                @endforeach
+                                            </select>
+                                            @error('subject')
+                                                <div class="text-danger">{{ $message }}</div>
+                                            @enderror
+                                        </div>
                                        
-                                        <div class="col-md-6">
+                                        <div class="col-md-3">
                                             <label for="exampleInputEmail1">status : </label>
                                             <select class="form-control" name="status">
                                                  <option value=" ">Select Option</option>
-                                                 <option value="active">Active</option>
-                                                 <option value="inactive">Inactive</option>
+                                                 <option value="1">Active</option>
+                                                 <option value="0">Inactive</option>
                                             </select>
                                             @error('status')
                                                 <div class="text-danger">{{ $message }}</div>
