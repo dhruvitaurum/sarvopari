64f3f4833d891f6dd1dc33c836f0542e4467c394
dhruvitsagar19 <159109135+dhruvitsagar19@users.noreply.github.com>
2024-02-21 19:18:21 +0530

    login page change

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,75 +1,105 @@
 <!DOCTYPE html>
-<html lang="en"> 
+<html lang="en">
 <head>
-    <title>Login</title>
-    
-    <!-- Meta -->
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <meta name="description" content="Login">
-    <meta name="author" content="Xiaoying Riley at 3rd Wave Media">    
-    <link rel="shortcut icon" href="favicon.ico"> 
-    
-    <!-- FontAwesome JS-->
-    <script defer src="assets/plugins/fontawesome/js/all.min.js"></script>
-    
-    <!-- App CSS -->  
-    <link id="theme-style" rel="stylesheet" href="assets/css/portal.css">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>AdminLTE 3 | Log in</title>
 
-</head> 
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="{{asset('admin_assets/plugins/fontawesome-free/css/all.min.css')}}">
+  <!-- icheck bootstrap -->
+  <link rel="stylesheet" href="{{asset('admin_assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="{{asset('admin_assets/dist/css/adminlte.min.css')}}">
+</head>
+<body class="hold-transition login-page">
+<div class="login-box">
+  <div class="login-logo">
+    <a href="index2.html"><b>Sarvopari</b></a>
+  </div>
+  <!-- /.login-logo -->
+  <div class="card">
+    <div class="card-body login-card-body">
+      <p class="login-box-msg">Sign in to start your session</p>
 
-<body class="app app-login p-0">    	
-    <div class="row g-0 app-auth-wrapper">
-	    <div class="col-12 col-md-7 col-lg-12 auth-main-col text-center p-5">
-		    <div class="d-flex flex-column align-content-end">
-			    <div class="app-auth-body mx-auto">	
-				    <div class="app-auth-branding mb-4"><a class="app-logo" href="index.html"><img class="logo-icon me-2" src="assets/images/app-logo.svg" alt="logo"></a></div>
-					<h2 class="auth-heading text-center mb-5">Log in to Sarvopari</h2>
-			        <div class="auth-form-container text-start">
-                    <x-auth-session-status class="mb-4" :status="session('status')" />
-						<form class="auth-form login-form"  method="POST" action="{{ route('login') }}">   
-                        @csrf      
-							<div class="email mb-3">
-								<label class="sr-only" for="email">Email</label>
-								<input id="email" name="email" :value="old('email')" type="email" class="form-control email" placeholder="Email address" required="required">
+	  <x-auth-session-status class="mb-4" :status="session('status')" />
+						<form class="auth-form login-form"  method="POST" action="{{ route('login') }}">
+						@csrf     
+        <div class="input-group mb-3">
+		<input id="email" name="email" :value="old('email')" type="email" class="form-control email" placeholder="Email address" required="required">
                                 <x-input-error :messages="$errors->get('email')" class="mt-2" />
-                            </div><!--//form-group-->
-							<div class="password mb-3">
-								<label class="sr-only" for="password">Password</label>
-								<input id="password" name="password" type="password" class="form-control signin-password" placeholder="Password" required autocomplete="current-password">
-								<x-input-error :messages="$errors->get('password')" class="mt-2" />
-                                <div class="extra mt-3 row justify-content-between">
-									<div class="col-6">
-										<div class="form-check">
-											<input class="form-check-input" type="checkbox" value="" id="remember_me" name="remember">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-envelope"></span>
+            </div>
+          </div>
+        </div>
+        <div class="input-group mb-3">
+		<input id="password" name="password" type="password" class="form-control signin-password" placeholder="Password" required autocomplete="current-password">
+		<x-input-error :messages="$errors->get('password')" class="mt-2" />
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-lock"></span>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-8">
+            <div class="icheck-primary">
+              <input class="form-check-input" type="checkbox" value="" id="remember_me" name="remember">
 											<label class="form-check-label" for="remember_me">
 											Remember me
 											</label>
-										</div>
-									</div><!--//col-6-->
-									<div class="col-6">
-										<div class="forgot-password text-end">
-                                        @if (Route::has('password.request'))
-											<a  href="{{ route('password.request') }}">Forgot password?</a>
-                                            @endif    
-										</div>
-									</div><!--//col-6-->
-								</div><!--//extra-->
-							</div><!--//form-group-->
-							<div class="text-center">
-								<button type="submit" class="btn app-btn-primary w-100 theme-btn mx-auto">Log In</button>
-							</div>
-						</form>
-						
-						<!--<div class="auth-option text-center pt-5">No Account? Sign up <a class="text-link" href="signup.html" >here</a>.</div>-->
-					</div><!--//auth-form-container-->	
+            </div>
+          </div>
+          <!-- /.col -->
+          <div class="col-4">
+            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+          </div>
+          <!-- /.col -->
+        </div>
+      </form>
 
-			    </div>
-		    </div>
-	    </div>
-	    
+      <div class="social-auth-links text-center mb-3">
+        <p>- OR -</p>
+        <a href="#" class="btn btn-block btn-primary">
+          <i class="fab fa-facebook mr-2"></i> Sign in using Facebook
+        </a>
+        <a href="#" class="btn btn-block btn-danger">
+          <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
+        </a>
+      </div>
+      <!-- /.social-auth-links -->
+
+      <p class="mb-1">
+	  <div class="col-6">
+			<div class="forgot-password text-end">
+			@if (Route::has('password.request'))
+				<a  href="{{ route('password.request') }}">Forgot password?</a>
+				@endif    
+			</div>
+		</div>
+      </p>
+      <p class="mb-0">
+        <a href="#" class="text-center">Register a new membership</a>
+      </p>
     </div>
+    <!-- /.login-card-body -->
+  </div>
+</div>
+<!-- /.login-box -->
+
+<!-- jQuery -->
+<script src="{{asset('admin_assets/plugins/jquery/jquery.min.js')}}"></script>
+<!-- Bootstrap 4 -->
+<script src="{{asset('admin_assets/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<!-- AdminLTE App -->
+<script src="{{asset('admin_assets/dist/js/adminlte.min.js')}}"></script>
 </body>
-</html> 
+</html>
+
+
+
+</head> 
