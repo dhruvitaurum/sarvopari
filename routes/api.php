2ac62105ff294b3c0cef4911fc2eba5cb2994e9a
dhruvitsagar19 <159109135+dhruvitsagar19@users.noreply.github.com>
2024-02-23 12:35:05 +0530

    code push

--- routes/api.php
+++ routes/api.php
@@ -48,3 +48,4 @@ Route::post('/institude/upload-video', [VideoController::class, 'upload_video'])
 Route::post('/student/homescreen-student', [StudentController::class, 'homescreen_student'])->name('upload_Video.get');
 Route::post('/institude/get-base-table-detail', [InstituteApiController::class, 'get_institute_reponse'])->name('institude.get');
 Route::post('/institude/register-institute', [InstituteApiController::class, 'register_institute'])->name('institude.register');
+Route::post('/institude/get-institute', [InstituteApiController::class, 'get_institute'])->name('get_institude.get');
