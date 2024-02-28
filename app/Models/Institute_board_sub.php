c4cb2742e708ad0634355d946f1afc1cee7b92e5
dhruvitsagar19 <159109135+dhruvitsagar19@users.noreply.github.com>
2024-02-28 18:33:51 +0530

    get home screen api

--- app/Models/Institute_board_sub.php
+++ app/Models/Institute_board_sub.php
@@ -4,11 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
+// use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Institute_board_sub extends Model
 {
-    use HasFactory,SoftDeletes;
+    use HasFactory;
     protected $table = 'board_sub';
     protected $fillable = [
         'user_id', 'institute_id', 'board_id', 'created_at', 'updated_at'
